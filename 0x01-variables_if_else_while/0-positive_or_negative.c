f7ed0981abb2799e8b3995bf81fe5cdcd472677d
Khalil Tahoun <khaliltahoun@gmail.com>
2023-03-17 18:32:30 -0700

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,15 +4,6 @@
 
 #include <time.h>
 
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
 
 int main(void)
 
@@ -24,7 +15,6 @@ srand(time(0));
 
 n = rand() - RAND_MAX / 2;
 
-/* your code goes there */
 
 if (n > 0)
 
