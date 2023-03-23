be50ad3be6e71788a67f56dade38e4d7c4b47dd3
Khalil Tahoun <khaliltahoun@gmail.com>
2023-03-23 07:26:50 -0700

    mandatory

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+                printf("%d is positive\n", n);
+	else if (i < 0)
+                printf("%d is negative\n", n);
+	else (n == 0)
+                printf("%d is zero\n", n);
+
+return (0);
+
+}
